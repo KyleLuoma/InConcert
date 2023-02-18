565443744c0febb93e3afe79ed0bdd5d2058889f
Kyle <kyle@jp8.dev>
2023-02-18 23:25:03 +0000

    added a buffer watcher to detect updates to buffers

--- aggregator-broadcaster/aggregator.c
+++ aggregator-broadcaster/aggregator.c
@@ -17,27 +17,65 @@ void main() {
 
     pthread_t udp_listener_thread, udp_broadcaster_thread, 
               tempo_calculator_thread, event_manager_thread, 
-              screen_display_thread;
+              screen_display_thread, buffer_watcher_thread;
 
     int udp_listener_t_ret, udp_broadcaster_t_ret,
         tempo_calculator_t_ret, event_manager_t_ret,
-        screen_display_t_ret;
+        screen_display_t_ret, buffer_watcher_t_ret;
 
-    int *receive_buffer = malloc(BUFFER_SIZE);
-    int *send_buffer = malloc(BUFFER_SIZE);
+    
+    int *send_buffer = malloc(SEND_BUFFER_SIZE);
+    struct tempo_message *tempo_buffer = malloc(TEMPO_BUFFER_SIZE * sizeof(struct tempo_message));
+    struct event_message *event_buffer = malloc(EVENT_BUFFER_SIZE * sizeof(struct event_message));
+
+    struct shared_buffer_stats buffer_stats;
+    
+    init_buffer_stats(&buffer_stats);
 
     struct udp_listener_t_arg listener_arg;
-    listener_arg.rcv_buffer = receive_buffer;
+    listener_arg.tempo_buffer = tempo_buffer;
+    listener_arg.event_buffer = event_buffer;
+    listener_arg.shared_buffer_stats = &buffer_stats;
 
-    udp_listener_t_ret = pthread_create( &udp_listener_thread, NULL, &udp_listener, &listener_arg);
+    udp_listener_t_ret = pthread_create(&udp_listener_thread, NULL, &udp_listener, &listener_arg);
+    buffer_watcher_t_ret = pthread_create(&buffer_watcher_thread, NULL, &buffer_watcher, &listener_arg);
 
     while(1) {};
 }
 
+void init_buffer_stats(void *buffer_stats) {
+    struct shared_buffer_stats *stats = buffer_stats; 
+    stats->tempo_buffer_last_write_ix = 0;
+    stats->tempo_buffer_last_read_ix = 0;
+    stats->tempo_buffer_locked = 0;
+    stats->event_buffer_last_write_ix = 0;
+    stats->event_buffer_last_read_ix = 0;
+    stats->event_buffer_locked = 0;
+}
+
+//Takes in the same argument as the udp_listener and monitors stats to determine
+//if an update occurred to buffers
+static void * buffer_watcher(void *arg) {
+    struct udp_listener_t_arg *args = arg;
+    int tb_last_write = args->shared_buffer_stats->tempo_buffer_last_write_ix;
+    while(1) {
+        if(tb_last_write < args->shared_buffer_stats->tempo_buffer_last_write_ix) {
+            fprintf(stdout, "buffer_watcher detected a tempo buffer update with payload:\n");
+            struct tempo_message msg = args->tempo_buffer[args->shared_buffer_stats->tempo_buffer_last_write_ix];
+            fprintf(stdout, "     message_type: %i, bpm: %i, confidence: %i, timestamp: %lld\n", 
+                msg.message_type, msg.bpm, msg.confidence, msg.timestamp
+            );
+            tb_last_write = args->shared_buffer_stats->tempo_buffer_last_write_ix;
+        }
+    }
+}
 
 static void * udp_listener(void *arg) {
 
+    int *receive_buffer = malloc(RECEIVE_BUFFER_SIZE);
+
     struct udp_listener_t_arg *listener_arg = arg;
+    struct shared_buffer_stats *buffer_stats = listener_arg->shared_buffer_stats;
 
     fprintf(stdout, "\n UDP listener running \n");
     int sockfd;
@@ -70,14 +108,11 @@ static void * udp_listener(void *arg) {
         fprintf(stdout, "error binding to socket \n");
     }
 
-    buf = malloc(BUFFER_SIZE);
-
     while(1) {
 
         msg_type = -1;
 
-        memset(buf, 0, BUFFER_SIZE);
-        n = recvfrom(sockfd, listener_arg->rcv_buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientaddr, &clientlen);
+        n = recvfrom(sockfd, receive_buffer, RECEIVE_BUFFER_SIZE, 0, (struct sockaddr *)&clientaddr, &clientlen);
         if(n < 0) {
             fprintf(stdout, "\nrecvfrom error \n");
         } else {
@@ -100,7 +135,7 @@ static void * udp_listener(void *arg) {
             //How to read from buffer:
             //htonl(*(listener_arg->rcv_buffer + i))
 
-            msg_type = htonl(*(listener_arg->rcv_buffer));
+            msg_type = htonl(*(receive_buffer));
 
             fprintf(stdout, "\nMessage type: %i\n", msg_type);
         }
@@ -109,17 +144,53 @@ static void * udp_listener(void *arg) {
 
         if(msg_type == TEMPO) {
             fprintf(stdout, "Received tempo message!\n");
+            struct tempo_message msg;
+
+            msg.message_type = msg_type;
+            msg.bpm = htonl(*(receive_buffer + 1));
+            msg.confidence = htonl(*(receive_buffer + 2));
+            msg.timestamp = htonl(*(receive_buffer + 3));
+
+            write_to_tempo_buffer(listener_arg->tempo_buffer, msg, buffer_stats);
         } else if(msg_type == EVENT) {
             fprintf(stdout, "Received event message!\n");
         } else if(msg_type == TIME) {
             fprintf(stdout, "Received time message!\n");
         }
 
-        n = sendto(sockfd, listener_arg->rcv_buffer, n, 0, (struct sockaddr*)&clientaddr, clientlen);
+        n = sendto(sockfd, receive_buffer, n, 0, (struct sockaddr*)&clientaddr, clientlen);
         if(n < 0){
             //do error stuff
         }
     }
+}
+
+//Write data in message to tempo buffer
+//Use parameters in buffer stats to write to correct location or reject request if locked
+//Return -1 if failure, else return write index
+int write_to_tempo_buffer(void *tempo_buffer, struct tempo_message message, void *buffer_stats) {
+
+    struct shared_buffer_stats *stats = buffer_stats;
+    struct tempo_message *tempo_buff = tempo_buffer;
+
+    if(stats->tempo_buffer_locked == 1) {
+        fprintf(stdout, "tempo buffer locked!");
+        return -1;
+    } else {
+        fprintf(stdout, "Writing tempo to buffer\n");
+        fprintf(stdout, "Message type: %i, BPM: %i, Confidence: %i, timestamp: %llu\n", 
+            message.message_type,
+            message.bpm,
+            message.confidence,
+            message.timestamp
+        );
+        stats->tempo_buffer_locked = 1;
+        stats->tempo_buffer_last_write_ix++;
+        tempo_buff[stats->tempo_buffer_last_write_ix] = message;
+        stats->tempo_buffer_locked = 0;
+        fprintf(stdout, "Wrote message to buffer position %i\n", stats->tempo_buffer_last_write_ix);
+    }
+
 }
 
 // udp_listener() Adapted from:
--- aggregator-broadcaster/aggregator.h
+++ aggregator-broadcaster/aggregator.h
@@ -1,11 +1,15 @@
 
 #define PORT 54523
-#define BUFFER_SIZE 1024
+#define RECEIVE_BUFFER_SIZE 1024 //bytes
+#define SEND_BUFFER_SIZE 1024 //bytes
+#define EVENT_BUFFER_SIZE 64 //Number of events
+#define TEMPO_BUFFER_SIZE 64 //Number of events
 
 //Message types:
 #define TEMPO 0
 #define EVENT 1
 #define TIME  2
+
 
 //Message structs:
 struct tempo_message {
@@ -28,12 +32,31 @@ struct time {
     long long timestamp;
 };
 
-void main();
-
-static void * udp_listener(void *listener_arg);
 
 struct udp_listener_t_arg {
-    int *rcv_buffer;
-    struct tempo_message *tempo_buffer;
-    struct event_message *event_buffer;
+    struct tempo_message        *tempo_buffer;
+    struct event_message        *event_buffer;
+    struct shared_buffer_stats  *shared_buffer_stats;
 };
+
+struct shared_buffer_stats {
+    int tempo_buffer_last_write_ix;
+    int tempo_buffer_last_read_ix;
+    int tempo_buffer_locked;
+
+    int event_buffer_last_write_ix;
+    int event_buffer_last_read_ix;
+    int event_buffer_locked;
+};
+
+
+void main();
+void init_buffer_stats(void *buffer_stats);
+int write_to_tempo_buffer(
+    void *tempo_buffer,
+    struct tempo_message message,
+    void *buffer_stats
+    );
+
+static void * udp_listener(void *listener_arg);
+static void * buffer_watcher(void *arg);
