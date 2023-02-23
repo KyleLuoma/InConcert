4a040d6ae9f10ed172e363fe1722c59929b00047
Kyle Luoma <kluoma@ucsd.edu>
2023-02-22 16:59:37 -0800

    drum senses inputs using ads1115

--- sensor-drum/sensor-drum.ino
+++ sensor-drum/sensor-drum.ino
@@ -1,33 +1,92 @@
+#include <Adafruit_ADS1X15.h>
 
-#define DRUM_IN A0
-#define SENSOR_DELAY 20
-#define THRESHOLD_LOW 200
-#define THRESHOLD_HIGH 360
+#define SNARE 0
+#define BASS 1
+#define CYMBAL 2
+#define TOM 3
 
-#include <netinet/in.h>
+#define SAMPLE_INTERVAL 2  //MS between sensor samples (x4 samples)
+#define INIT_CYCLES 100    //Number of sample cycles to perform to evaluate baseline
+#define INPUT_THRESHOLD 50 //Amount of signal to increase above idle to register as a hit
 
-int val_vib_0 = 0;
+Adafruit_ADS1115 ads;
+
+int16_t snare, cymbal, bass, tom; //Use for storing sample inputs
+int16_t snare_th, cymbal_th, bass_th, tom_th;//Input thresholds
+const int ledPin = LED_BUILTIN; 
+int led_state = LOW;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.print("a0, a1, a2, a3\n"); 
+  Serial.begin(9600);
+  Serial.print("Initializing drum-sensor runtime\n"); 
+
+  if(!ads.begin()) {
+    Serial.print("Unable to start ADS. Check the connections and try again.");
+  }
+
+  pinMode(ledPin, OUTPUT);
+
+  int16_t max_s = 0;
+  int16_t max_b = 0;
+  int16_t max_c = 0;
+  int16_t max_tom = 0;
+  //document baseline signal - no events
+  //user should not hit drums during initialization period
+  for(int i = 0; i < INIT_CYCLES; i++) {
+    if(led_state == LOW){
+      led_state = HIGH;
+    } else {
+      led_state = LOW;
+    }
+    digitalWrite(ledPin, led_state);
+    snare = ads.readADC_SingleEnded(SNARE);
+    if (snare > max_s){
+      max_s = snare;
+    }
+    delay(SAMPLE_INTERVAL);
+    bass = ads.readADC_SingleEnded(BASS);
+    if(bass > max_b){
+      max_b = bass;
+    }
+    delay(SAMPLE_INTERVAL);
+    digitalWrite(ledPin, HIGH);
+    cymbal = ads.readADC_SingleEnded(CYMBAL);
+    if(cymbal > max_c){
+      max_c = cymbal;
+    }
+    delay(SAMPLE_INTERVAL);
+    tom = ads.readADC_SingleEnded(TOM);
+    if(tom > max_tom){
+      max_tom = tom;
+    }
+    delay(SAMPLE_INTERVAL);    
+  }
+  snare_th = max_s + INPUT_THRESHOLD;
+  cymbal_th = max_c + 10;
+  bass_th = max_b + INPUT_THRESHOLD;
+  tom_th = max_tom + INPUT_THRESHOLD;
 }
 
-int start = millis();
-int next_sample = start + SENSOR_DELAY;
-int sample_time;
-
 void loop() {
-  sample_time = millis();
-  if(sample_time > next_sample) {
-    val_vib_0 = analogRead(DRUM_IN);
-//    Serial.print(sample_time);
-//    Serial.print(", ");
-    if(val_vib_0 > THRESHOLD_LOW && val_vib_0 < THRESHOLD_HIGH) {
-      val_vib_0 = 280;
-    }
-    Serial.print(val_vib_0);
-    Serial.print("\n");
-    next_sample += SENSOR_DELAY;
-  } 
+  snare = ads.readADC_SingleEnded(SNARE);
+  delay(SAMPLE_INTERVAL);
+  bass = ads.readADC_SingleEnded(BASS);
+  delay(SAMPLE_INTERVAL);
+  cymbal = ads.readADC_SingleEnded(CYMBAL);
+  delay(SAMPLE_INTERVAL);
+  tom = ads.readADC_SingleEnded(TOM);
+  delay(SAMPLE_INTERVAL);
+
+  if(snare > snare_th || cymbal > cymbal_th || bass > bass_th || tom > tom_th){
+    led_state = HIGH;
+  } else {
+    led_state = LOW;
+  }
+  digitalWrite(ledPin, led_state);
+
+  Serial.print(snare); Serial.print(", ");
+  Serial.print(bass); Serial.print(", ");
+  Serial.print(cymbal); Serial.print(", ");
+  Serial.print(tom); Serial.print("\n");
+
 }
