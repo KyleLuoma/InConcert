184d2ef3b895c109b4a07fbe19ab7d7cb09e4d7d
Kyle Luoma <kluoma@ucsd.edu>
2023-02-25 21:08:29 -0800

    working on implementing registration

--- sensor-drum/inconcert-communication.h
+++ sensor-drum/inconcert-communication.h
@@ -1,5 +1,8 @@
 #include <stdint.h>
 
+#define INCLUDE_HTONX
+
+#ifdef INCLUDE_HTONX
 #define htons(x) ( ((x)<< 8 & 0xFF00) | \
                    ((x)>> 8 & 0x00FF) )
 
@@ -7,14 +10,18 @@
                    ((x)<< 8 & 0x00FF0000UL) | \
                    ((x)>> 8 & 0x0000FF00UL) | \
                    ((x)>>24 & 0x000000FFUL) )
+#endif
 
 
 #define SEND_PORT 54523
+#define BROADCAST_PORT 54555
+#define BROADCAST_IP "10.42.0.255"
 
 //Message types:
 #define TEMPO 0
 #define EVENT 1
 #define TIME  2
+#define REGISTER_CLIENT 3
 
 //Message structs:
 struct tempo_message {
@@ -26,21 +33,28 @@ struct tempo_message {
     uint32_t beat;
 };
 
-struct event_message {
+struct event_message { //size 16x uint32_t
     uint32_t message_type;
     uint32_t device_id;
     uint32_t event_type;
     uint32_t measure;
     uint32_t beat;
-    uint32_t num_params;
-    uint32_t *params;
+    uint32_t num_used_params; //1-10
+    uint32_t params[10];
 };
 
-struct time {
+struct time_message { //Size 7x uint32_t
     uint32_t message_type;
     uint32_t device_id;
     uint32_t beat_signature_L;
     uint32_t beat_signature_R;
     uint32_t measure;
     uint32_t beat; //1 ... beat_signature_R values
+    uint32_t beat_interval; //ms between beats
 };
+
+struct register_client_message {
+    uint32_t message_type;
+    uint32_t device_id;
+};
+
--- sensor-drum/sensor-drum.ino
+++ sensor-drum/sensor-drum.ino
@@ -17,6 +17,8 @@
 #define SAMPLE_INTERVAL 2  //MS between sensor samples (x4 samples)
 #define INIT_CYCLES 100    //Number of sample cycles to perform to evaluate baseline
 #define INPUT_THRESHOLD 50 //Amount of signal to increase above idle to register as a hit
+#define IGNORE_PERIOD 30 //Number of samples to ignore after a hit detection
+#define UDP_READ_TIMEOUT 1000 //Number of ms until timing out on UDP read
 
 // #define SERIAL_PRINT
 
@@ -29,11 +31,18 @@ int led_state = LOW;
 int has_wifi = 1;
 int wifi_status;
 int i;
+int16_t cur_measure, cur_beat;
 
 uint32_t tempo_msg_buffer[5];
+uint32_t incoming_udp_buffer[24]; //big enough to handle time or event messages
+uint32_t registration_message_buffer[2];
+
+struct time_message current_time;
 
 IPAddress AGGServer(10, 42, 0, 1); // Aggregator wifi hotspot address
-WiFiUDP Udp;
+WiFiUDP udp_out;
+IPAddress broadcast_ip(10, 42, 0, 255); 
+WiFiUDP udp_in;
 const int AGG_PACKET_SIZE = 255;
 
 void setup() {
@@ -65,7 +74,12 @@ void setup() {
       }
     }
     Serial.println("\nStarting connection to server...");
-    Udp.begin(SEND_PORT); 
+    if(udp_out.begin(SEND_PORT)==0){
+      Serial.print("Unable to bind to send socket.\n");
+    } 
+    if(udp_in.begin(BROADCAST_PORT)==0){
+      Serial.print("Unable to bind receive port.\n");
+    };
   }
 
   pinMode(ledPin, OUTPUT);
@@ -111,20 +125,19 @@ void setup() {
   tom_th = max_tom + INPUT_THRESHOLD;
 }
 
-//Operates on a periodic schedule:
-// ms   event
-// 0    Sample Snare
-// 1    Send previously calculated tempo
-// 2    Sample Bass
-// 3    Get network time synch
-// 4    Sample HiHat
-// 5    Send events in queue
-// 6    Sample Tom
-// 7    Recalculate tempo
-// 8    Write to terminal
-
 unsigned long loop_start_time;
 unsigned long next_sample;
+
+unsigned long registerWithHost(IPAddress& address) {
+  Serial.print("Sending registration message to Agg Server ");
+  Serial.println(AGGServer);
+  memset(registration_message_buffer, 0, sizeof(struct register_client_message));
+  registration_message_buffer[0] = htonl(REGISTER_CLIENT);
+  registration_message_buffer[1] = htonl(DEVICE_ID);
+  udp_out.beginPacket(address, SEND_PORT);
+  udp_out.write((uint8_t*)registration_message_buffer, sizeof(struct register_client_message));
+  udp_out.endPacket();
+}
 
 unsigned long sendAGGpacket(IPAddress& address) {
   Serial.print("Sending Packet to Agg Server ");
@@ -138,34 +151,95 @@ unsigned long sendAGGpacket(IPAddress& address) {
   tempo_msg_buffer[3] = htonl(94);           //confidence
   tempo_msg_buffer[5] = htonl(10001);        //timestamp
 
-  Udp.beginPacket(address, SEND_PORT); //AGG requests are to port 54534
-  Udp.write((uint8_t*)tempo_msg_buffer, sizeof(struct tempo_message));
-  Udp.endPacket();
+  udp_out.beginPacket(address, SEND_PORT); //AGG requests are to port 54534
+  udp_out.write((uint8_t*)tempo_msg_buffer, sizeof(struct tempo_message));
+  udp_out.endPacket();
 }
+
+int getTimeMessage(struct time_message * new_time) {
+  unsigned long timeout = millis() + UDP_READ_TIMEOUT;
+  int got_message = 0;
+  Serial.print("Waiting for time message\n");
+  udp_in.flush();
+  while(got_message == 0 && millis() < timeout){
+  // while(got_message == 0) {
+    if(udp_in.parsePacket() > 0){
+      Serial.print("Received a UDP packet, checking type.\n");
+      udp_in.read((unsigned char*)incoming_udp_buffer, sizeof(incoming_udp_buffer));
+      if(((uint32_t*)incoming_udp_buffer)[0] == htonl(TIME)){
+        new_time->message_type     = incoming_udp_buffer[0];
+        new_time->device_id        = incoming_udp_buffer[1];
+        new_time->beat_signature_L = incoming_udp_buffer[2];
+        new_time->beat_signature_R = incoming_udp_buffer[3];
+        new_time->measure          = incoming_udp_buffer[4];
+        new_time->beat             = incoming_udp_buffer[5];
+        new_time->beat_interval    = incoming_udp_buffer[6];
+        got_message = 1;
+        Serial.print("Received time message "); Serial.print(new_time->measure);
+        Serial.print(new_time->beat); Serial.print("\n");
+      }
+    } else {
+      Serial.print("Received UDP message that wasn't a TIME message\n");
+      udp_in.flush();
+    }
+  }
+  if(millis() > UDP_READ_TIMEOUT){
+    Serial.print("Timed out while waiting for udp packets on broadcast.\n");
+    registerWithHost(AGGServer);
+  }
+}
+
+//Operates on a periodic schedule:
+// ms   event
+// 0    Sample Snare
+// 1    Send previously calculated tempo
+// 2    Sample Bass
+// 3    Get network time synch
+// 4    Sample HiHat
+// 5    Send events in queue
+// 6    Sample Tom
+// 7    Recalculate tempo
+// 8    Write to terminal
+
 unsigned int loopcounter = 0;
 void loop() {
   loop_start_time = micros();
 
+  //0 Sample Snare
   snare = ads.readADC_SingleEnded(SNARE);
   next_sample = loop_start_time + 2000;
 
+  //1 Send tempo packet
   if(has_wifi == 1 && loopcounter % 125 == 0){
     sendAGGpacket(AGGServer);
   }
 
+  //2 Sample bass  
   while(micros() < next_sample){};
   bass = ads.readADC_SingleEnded(BASS);
   next_sample = loop_start_time + 4000;
 
+  //3 Get time synch
+  if(has_wifi == 1 && (loopcounter + 50) % 125 == 0){
+    getTimeMessage(&current_time);    
+  }
+
+  //4 Sample hihat  
   while(micros() < next_sample){};
   cymbal = ads.readADC_SingleEnded(CYMBAL);
   next_sample = loop_start_time + 6000;
 
+  //5 Send events in queue
+
+  //6 Sample Tom
   while(micros() < next_sample){};
   tom = ads.readADC_SingleEnded(TOM);
   next_sample = loop_start_time + 8000;
 
+  // 7 Recalculate tempo
 
+
+  // 8 Flash LED and Write to terminal
   if(snare > snare_th || cymbal > cymbal_th || bass > bass_th || tom > tom_th){
     led_state = HIGH;
   } else {
