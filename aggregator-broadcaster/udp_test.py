84fdec52e61ed7efce40d029be619788136e5a87
kyleluoma <kyle@jp8.dev>
2023-03-03 13:17:06 -0800

    implemented basic back-forth led animation

--- aggregator-broadcaster/udp_test.py
+++ aggregator-broadcaster/udp_test.py
@@ -1,11 +1,12 @@
 import socket
 from struct import *
 
-HOST_IP = "10.42.0.1"
+# HOST_IP = "10.42.0.1"
+HOST_IP = "35.92.239.128"
 HOST_PORT = 54523
 
 # message_type, device_id, bpm, confidence, measure, beat
-TP = pack("!iiiiiq", 0 ,23, 60,89,4,4)
+TP = pack("!iiiiiq", 0 ,23, 62,89,4,4)
 EP = pack("!iiiiiiiii", 1, 1004, 1, 4, 2, 3, 1, 2, 3)
 REG = pack("!ii", 3, 1005)
 
@@ -15,7 +16,7 @@ sock_test = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # response = sock_test.sendto(EP, (HOST_IP, HOST_PORT))
 # print(response)
 
-response = sock_test.sendto(TP, (HOST_IP, HOST_PORT))
+response = sock_test.sendto(REG, (HOST_IP, HOST_PORT))
 print(response)
 
 # response = sock_test.sendto(REG, (HOST_IP, HOST_PORT))
--- metronome/metronome.ino
+++ metronome/metronome.ino
@@ -1,9 +1,17 @@
 #include <SPI.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+
+#include "wifi-info.h"
 
 #define NUM_LEDS 7
 
 int led_pin_array[NUM_LEDS] = {2,3,4,5,6,7,8};
 int led_status_array[NUM_LEDS] = {LOW};
+
+char ssid[] = SSID;
+char pw[] = PW;
+int wifi_status = WL_IDLE_STATUS;
 
 void setup() {
     Serial.print("Initializing LED array.\n");
@@ -16,15 +24,94 @@ void setup() {
         digitalWrite(led_pin_array[i], led_status_array[i]);
         delay(100);
     }
+
+    Serial.print("Setting up WIFI connection.\n");
+    if(WiFi.status() == WL_NO_MODULE){
+        Serial.print("Can't seem to find the wifi module :( \n");
+        digitalWrite(led_pin_array[0], HIGH);
+        digitalWrite(led_pin_array[6], HIGH);
+        while(true);
+    }
+
+    int counter = 0;
+
+    while(wifi_status != WL_CONNECTED) {
+        Serial.print("Connecting to InConcert access point.\n");
+        wifi_status = WiFi.begin(ssid, 0, pw);
+        all_leds_off();
+        
+        while(counter < 10){
+            led_status_array[3] = HIGH;
+            refresh_leds();
+            delay(500);
+            led_status_array[3] = LOW;
+            refresh_leds();
+            delay(500);
+            counter++;
+        }
+        counter = 0;
+    }
+    Serial.print("Connected!\n");
+    all_leds_off();
+    while(counter < 3){
+        led_status_array[2] = HIGH;
+        led_status_array[4] = HIGH;
+        refresh_leds();
+        delay(500);
+        led_status_array[2] = LOW;
+        led_status_array[4] = LOW;
+        refresh_leds();
+        delay(500);
+        counter++;
+    }
+    counter = 0;
 }
 
-void loop() {
+void all_leds_off() {
+    for(int i = 0; i < NUM_LEDS; i++){
+        led_status_array[i] = LOW;
+        digitalWrite(led_pin_array[i], led_status_array[i]);
+    }
+}
+
+void all_leds_on() {
     for(int i = 0; i < NUM_LEDS; i++){
         led_status_array[i] = HIGH;
         digitalWrite(led_pin_array[i], led_status_array[i]);
-        delay(100);
-        led_status_array[i] = LOW;
-        digitalWrite(led_pin_array[i], led_status_array[i]);
-        delay(100);
     }
+}
+
+
+void refresh_leds() {
+    for(int i = 0; i < NUM_LEDS; i++){
+        digitalWrite(led_pin_array[i], led_status_array[i]);
+    }
+}
+
+int active_led = 0; //Which led is active 
+int direction = 1; //Direction of movement
+int interval = 50; //Time between movements
+int bpm = 120;
+unsigned long next_movement = millis() + interval;
+void loop() {
+
+    interval = (60000/bpm) / (NUM_LEDS - 1);
+    
+    if(millis() > next_movement) {
+        // move led back and forth
+        led_status_array[active_led] = LOW;
+        if(active_led < NUM_LEDS && active_led >= 0) {
+            active_led += direction;
+        } 
+        if(active_led == 0){
+            direction = 1;
+        } 
+        if(active_led == (NUM_LEDS - 1)) {
+            direction = -1;
+        }
+        led_status_array[active_led] = HIGH;
+        refresh_leds();
+        next_movement += interval;
+    }
+    
 }
